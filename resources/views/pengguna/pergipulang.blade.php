5cc17bca55a2eea0408fde31244e692b524c6d36
XenonightX <adjayaandre@gmail.com>
2024-05-22 12:13:05 +0700

    menambahkan icon calendar dan swipe, serta menambahkan view pergipulang.php dan sekalipergi.php

---
+++ resources/views/pengguna/pergipulang.blade.php
@@ -0,0 +1,177 @@
+@extends('layout.main')
+
+@section('container')
+<main>
+
+    <!-- loc -->
+    <div class="flex flex-wrap justify-between ">
+    <div class="flex flex-auto items-start mx-auto">
+        <img src="/img/icons/harbor.png" class="w-6 ms-2 pr-2">
+        <span href="#" class="text-white  px-2 text-sm font-medium bg-neutral-400 rounded-sm">Batam</span>
+        <span href="#" class="px-2 text-sm font-medium"><button class="hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300"><img src="/img/icons/swap.png" class="w-4"></span></button>
+        <span href="#" class="text-white  px-2 text-sm font-medium bg-neutral-400 rounded-sm">Karimun</span>
+    </div>
+
+    <div class="flex flex-auto mx-auto justify-end">
+        <img src="/img/icons/calendar.png" class="w-6 ms-2 pr-2">
+        <span href="#" class="text-white  px-2 text-sm font-medium bg-neutral-400 rounded-sm">26/03/2024</span>
+        <span href="#" class="px-2 text-sm font-medium"><button class="hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300"><img src="/img/icons/swap.png" class="w-4"></span></button>
+        <span href="#" class="text-white  px-2 text-sm font-medium bg-neutral-400 rounded-sm">30/03/2024</span>
+    </div>
+    </div><br>
+<!-- container -->
+<div class="container booking ">
+    <div class="container justify-content-center pt-3 pb-3 bg-gradient-to-tr from-[#ffebe1] to-[#DEC3B4]"
+        style="max-width: 1300px; width: 100%; background-color:#c2c1c1; border-radius:30px;">
+        <div class="container flex flex-row space-x-5 mx-4 pt-2">
+            <img src="/img/icons/ships.png" class="w-12">
+            <div class="flex flex-row">
+                Nongsapura Feri Terminal, Batam <br>
+                50m Perjalanan Feri Batam Feri Cepat Kelas Ekonomi
+            </div>
+            <div class="justify-items-end pe-2">
+                08 : 00 - 09 : 50
+            </div>
+            <div class="justify-items-end pe-5">
+                Tanah Merah Feri Terminal, Singapore
+            </div>
+            <div class="flex flex-row">
+                Rp 54.000,00
+            </div>
+            <div><a href="/login"
+                    class="font-sans inline-flex items-center px-3 py-2 text-sm font-medium text-center text-dark bg-[#DEC3B4] rounded-lg hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    Pesan Tiket
+                </a>
+            </div>
+</div>
+</div>
+            <br>
+            <div class="border border-b-black">
+            </div><br>
+</div>
+
+<div class="container booking ">
+    <div class="container justify-content-center pt-3 pb-3 bg-gradient-to-tr from-[#ffebe1] to-[#DEC3B4]"
+        style="max-width: 1300px; width: 100%; background-color:#c2c1c1; border-radius:30px;">
+        <div class="container flex flex-row space-x-5 mx-4 pt-2">
+            <img src="/img/icons/ships.png" class="w-12">
+            <div class="flex flex-row">
+                Nongsapura Feri Terminal, Batam <br>
+                50m Perjalanan Feri Batam Feri Cepat Kelas Ekonomi
+            </div>
+            <div class="justify-items-end pe-2">
+                08 : 00 - 09 : 50
+            </div>
+            <div class="justify-items-end pe-5">
+                Tanah Merah Feri Terminal, Singapore
+            </div>
+            <div class="flex flex-row">
+                Rp 54.000,00
+            </div>
+            <div><a href="/login"
+                    class="font-sans inline-flex items-center px-3 py-2 text-sm font-medium text-center text-dark bg-[#DEC3B4] rounded-lg hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    Pesan Tiket
+                </a>
+            </div>
+</div>
+</div>
+            <br>
+            <div class="border border-b-black">
+            </div><br>
+</div>
+
+<div class="container booking ">
+    <div class="container justify-content-center pt-3 pb-3 bg-gradient-to-tr from-[#ffebe1] to-[#DEC3B4]"
+        style="max-width: 1300px; width: 100%; background-color:#c2c1c1; border-radius:30px;">
+        <div class="container flex flex-row space-x-5 mx-4 pt-2">
+            <img src="/img/icons/ships.png" class="w-12">
+            <div class="flex flex-row">
+                Nongsapura Feri Terminal, Batam <br>
+                50m Perjalanan Feri Batam Feri Cepat Kelas Ekonomi
+            </div>
+            <div class="justify-items-end pe-2">
+                08 : 00 - 09 : 50
+            </div>
+            <div class="justify-items-end pe-5">
+                Tanah Merah Feri Terminal, Singapore
+            </div>
+            <div class="flex flex-row">
+                Rp 54.000,00
+            </div>
+            <div><a href="/login"
+                    class="font-sans inline-flex items-center px-3 py-2 text-sm font-medium text-center text-dark bg-[#DEC3B4] rounded-lg hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    Pesan Tiket
+                </a>
+            </div>
+</div>
+</div>
+            <br>
+            <div class="border border-b-black">
+            </div><br>
+</div>
+
+<div class="container booking ">
+    <div class="container justify-content-center pt-3 pb-3 bg-gradient-to-tr from-[#ffebe1] to-[#DEC3B4]"
+        style="max-width: 1300px; width: 100%; background-color:#c2c1c1; border-radius:30px;">
+        <div class="container flex flex-row space-x-5 mx-4 pt-2">
+            <img src="/img/icons/ships.png" class="w-12">
+            <div class="flex flex-row">
+                Nongsapura Feri Terminal, Batam <br>
+                50m Perjalanan Feri Batam Feri Cepat Kelas Ekonomi
+            </div>
+            <div class="justify-items-end pe-2">
+                08 : 00 - 09 : 50
+            </div>
+            <div class="justify-items-end pe-5">
+                Tanah Merah Feri Terminal, Singapore
+            </div>
+            <div class="flex flex-row">
+                Rp 54.000,00
+            </div>
+            <div><a href="/login"
+                    class="font-sans inline-flex items-center px-3 py-2 text-sm font-medium text-center text-dark bg-[#DEC3B4] rounded-lg hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    Pesan Tiket
+                </a>
+            </div>
+</div>
+</div>
+            <br>
+            <div class="border border-b-black">
+            </div><br>
+</div>
+
+<div class="container booking ">
+    <div class="container justify-content-center pt-3 pb-3 bg-gradient-to-tr from-[#ffebe1] to-[#DEC3B4]"
+        style="max-width: 1300px; width: 100%; background-color:#c2c1c1; border-radius:30px;">
+        <div class="container flex flex-row space-x-5 mx-4 pt-2">
+            <img src="/img/icons/ships.png" class="w-12">
+            <div class="flex flex-row">
+                Nongsapura Feri Terminal, Batam <br>
+                50m Perjalanan Feri Batam Feri Cepat Kelas Ekonomi
+            </div>
+            <div class="justify-items-end pe-2">
+                08 : 00 - 09 : 50
+            </div>
+            <div class="justify-items-end pe-5">
+                Tanah Merah Feri Terminal, Singapore
+            </div>
+            <div class="flex flex-row">
+                Rp 54.000,00
+            </div>
+            <div><a href="/login"
+                    class="font-sans inline-flex items-center px-3 py-2 text-sm font-medium text-center text-dark bg-[#DEC3B4] rounded-lg hover:bg-[#DEC3B4] hover:scale-110 transition duration-150 hover:ease-out focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                    Pesan Tiket
+                </a>
+            </div>
+</div>
+</div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+</script>
+<br>
+
+</body>
+@endsection
