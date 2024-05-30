1ca7e07fe77276e2c0854ece05cd4e55525525c3
Yunita Caroline <yunitacaroline288@gmail.com>
2024-05-30 22:14:34 +0700

    perbaiki routes web.php

--- routes/web.php
+++ routes/web.php
@@ -9,107 +9,53 @@ use App\Http\Controllers\RegistrasiController;
 use App\Http\Controllers\InformasiController;
 use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\LoginController;
-<<<<<<< Updated upstream
 use App\Http\Controllers\TransaksiController;
 use App\Livewire\Kapal;
-
-=======
 use App\Http\Controllers\KapalController;
 use App\Http\Controllers\MailerController;
->>>>>>> Stashed changes
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-//Route::get('/welcome', function () {
-//return view('welcome');
-//});
-
-//Route::get('/user/{id}', function ($id) {
-//return 'User dengan ID ' . $id;
-//});
-
-//Route::prefix('admin')->group(function () {
-//    Route::get('/dashboard', function () {
-//        return 'Admin Dashboard';
-//    });
-
-//    Route::get('/users', function () {
-//        return 'Admin Users';
-//    });
-//});
-
-// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
-//    return view('list_barang', compact('id', 'nama'));
-// });
-
-//Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
 
 Route::get('/', function () {
     return view('landing');
 });
 
-
-Route::get('/admin/dashboard', function () {
-    return view('/admin/dashboard');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+    Route::get('/pelabuhan', function () {
+        return view('admin.pelabuhan');
+    });
+    Route::get('/kapal', function () {
+        return view('admin.kapal');
+    });
+    Route::get('/rute', function () {
+        return view('admin.rute');
+    });
+    Route::get('/tiket', function () {
+        return view('admin.tiket');
+    });
+    Route::get('/pengguna', function () {
+        return view('admin.pengguna');
+    });
+    Route::get('/pesanan', function () {
+        return view('admin.pesanan');
+    });
+    Route::get('/tambahkapal', function () {
+        return view('admin.formulir.tambahkapal');
+    });
+    Route::get('/tambahrute', function () {
+        return view('admin.formulir.tambahrute');
+    });
+    Route::get('/tambahtiket', function () {
+        return view('admin.formulir.tambahtiket');
+    });
+    Route::get('/tambahpelabuhan', function () {
+        return view('admin.formulir.tambahpelabuhan');
+    });
+    Route::get('/tambahpengguna', function () {
+        return view('admin.formulir.tambahpengguna');
+    });
 });
-
-//Route::get('/admin/kapal', function () {
-//    return view('/admin/kapal');
-//});
-
-Route::get('/admin/pelabuhan', function () {
-    return view('/admin/pelabuhan');
-});
-
-Route::get('/admin/rute', function () {
-    return view('/admin/rute');
-});
-
-Route::get('/admin/tiket', function () {
-    return view('/admin/tiket');
-});
-
-Route::get('/admin/pengguna', function () {
-    return view('/admin/pengguna');
-});
-
-Route::get('/admin/pesanan', function () {
-    return view('/admin/pesanan');
-});
-
-Route::get('/admin/tambahkapal', function () {
-    return view('/admin/formulir/tambahkapal');
-});
-
-Route::get('/admin/tambahrute', function () {
-    return view('/admin/formulir/tambahrute');
-});
-
-Route::get('/admin/tambahtiket', function () {
-    return view('/admin/formulir/tambahtiket');
-});
-
-Route::get('/admin/tambahpelabuhan', function () {
-    return view('/admin/formulir/tambahpelabuhan');
-});
-
-Route::get('/admin/tambahpengguna', function () {
-    return view('/admin/formulir/tambahpengguna');
-});
-
-Route::get('/list_barang', [ListBarangController::class, 'tampilkan']);
-
-//Route::get('/', [HomeController::class, 'index']);
-Route::get('/contact', [HomeController::class, 'contact']);
-
 
 // pengguna---------
 Route::get('/login', [LoginController::class, 'login']);
@@ -144,7 +90,6 @@ Route::get('/pesantiket2', function () {
 });
 
 //test kapal
-<<<<<<< Updated upstream
 
 Route::get('/admin/kapal', Kapal::class);
 // Route::get('/admin/kapal', [KapalController::class, 'kapal']);
@@ -153,14 +98,6 @@ Route::get('/admin/kapal', Kapal::class);
 // Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('delete');
 // Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('edit');
 // Route::post('/edit/{id}', [ProductController::class, 'update']);
-=======
-Route::get('/admin/kapal', [KapalController::class, 'kapal']);
-Route::get('/admin/tambahkapal', [KapalController::class, 'tambah'])->name('tambah');
-Route::post('admin/tambahkapal', [KapalController::class, 'simpan']);
-Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('delete');
-Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('edit');
-Route::post('/edit/{id}', [ProductController::class, 'update']);
 
 Route::get('send-mail', [MailerController::class, 'index'])->name('send.mail');
 Route::post('send-mail', [MailerController::class, 'store'])->name('send.email.post');
->>>>>>> Stashed changes
