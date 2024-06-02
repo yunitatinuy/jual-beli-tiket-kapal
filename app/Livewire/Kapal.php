a5316e5672ea453d8636342cde607818b588ed6f
Muhammad Dafa Putra <muhammaddavaputra@gmail.com>
2024-06-02 11:00:11 +0700

    Update CRUD
    Ready to Implementation for other table

--- app/Livewire/Kapal.php
+++ app/Livewire/Kapal.php
@@ -15,6 +15,8 @@ class Kapal extends Component
     public $jumlahkursi;
 
     public $search;
+    public $updateData = false;
+    public $kapalID;
 
     public function store()
     {
@@ -24,10 +26,18 @@ class Kapal extends Component
             'jumlahkursi' => 'required|integer',
         ];
 
-        $validated = $this->validate($rules);
+        $pesan = [
+            'namakapal.required' => 'Nama Kapal harus diisi.',
+            'kelas.required' => 'Kelas harus diisi.',
+            'jumlahkursi.required' => 'Jumlah Kursi harus diisi.',
+            'jumlahkursi.integer' => 'Jumlah Kursi diisi angka.',
+        ];
+
+        $validated = $this->validate($rules, $pesan);
         ModelsKapal::create($validated);
         $this->reset('namakapal', 'kelas', 'jumlahkursi');
         session()->flash('success', 'Data berhasil ditambahkan');
+        $this->resetPage();
     }
 
     public function delete($idkapal)
@@ -41,6 +51,54 @@ class Kapal extends Component
         }
     }
 
+    public function edit($idkapal)
+    {
+        $data = ModelsKapal::find($idkapal);
+        $this->namakapal = $data->namakapal;
+        $this->kelas = $data->kelas;
+        $this->jumlahkursi = $data->jumlahkursi;
+
+        $this->updateData = true;
+        $this->kapalID = $idkapal;
+    }
+
+    public function update()
+    {
+        $rules = [
+            'namakapal' => 'required',
+            'kelas' => 'required',
+            'jumlahkursi' => 'required|integer',
+        ];
+
+        $pesan = [
+            'namakapal.required' => 'Nama Kapal harus diisi.',
+            'kelas.required' => 'Kelas harus diisi.',
+            'jumlahkursi.required' => 'Jumlah Kursi harus diisi.',
+            'jumlahkursi.integer' => 'Jumlah Kursi diisi angka.',
+        ];
+
+        $validated = $this->validate($rules, $pesan);
+
+        $data = ModelsKapal::find($this->kapalID);
+        $data->update($validated);
+        session()->flash('message', 'Data berhasil diperbarui');
+
+        $this->reset('namakapal', 'kelas', 'jumlahkursi');
+
+        $this->clear();
+    }
+
+    public function clear()
+    {
+        $this->namakapal = '';
+        $this->kelas = '';
+        $this->jumlahkursi = '';
+
+        $this->updateData = false;
+        $this->kapalID = '';
+        $this->resetValidation();
+    }
+
     public function render()
     {
         // $data = ModelsKapal::orderBy('idkapal', 'asc')->paginate(5);
@@ -49,7 +107,7 @@ class Kapal extends Component
         $data = ModelsKapal::where('namakapal', 'like', $search)
             ->orWhere('kelas', 'like', $search)
             ->orWhere('jumlahkursi', 'like', $search)
-            ->orderBy('idkapal', 'asc')
+            ->orderBy('idkapal', 'desc')
             ->paginate(5);
 
         return view('livewire.kapal', ['dataKapal' => $data]);
--- resources/views/layout/adm.blade.php
+++ resources/views/layout/adm.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,13 +24,13 @@
 
 <body>
     <div class="mx-auto max-w-full">
-    @include('partials.admnav')
+        @include('partials.admnav')
 
-    @include('partials.sidebar')
+        @include('partials.sidebar')
 
-    @yield('container')
+        @yield('container')
 
-</div>
+    </div>
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 </body>
 
