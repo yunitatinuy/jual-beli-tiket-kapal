fcded886796fc129a599394a08af01ec72edb298
Muhammad Dafa Putra <muhammaddavaputra@gmail.com>
2024-06-26 22:47:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,12 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
     return redirect('/');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 // Logout
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
